eee705801f6b441e45290665d533ee2378c368c9
zzk <591858133@qq.com>
2021-07-14 15:51:25 +0800

    小改

--- userlist-service/src/test/java/com/cestc/vspace/userlistServiceTest.java
+++ userlist-service/src/test/java/com/cestc/vspace/userlistServiceTest.java
@@ -21,6 +21,7 @@ public class userlistServiceTest {
 	@Test
 	public void test() {
 		System.out.println(userservice.findById(2));
+		System.out.println("1");
 	}
 
 //	public static void main(String[] args) {
