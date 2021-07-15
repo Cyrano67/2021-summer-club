84b8221a10545c43cbd38138d501cb0e0ad8f803
zhong <1726384458@@qq.com>
2021-07-15 11:58:20 +0800

    test only

--- search-service/src/main/java/com/cestc/vspace/service/impl/SearchServiceImp.java
+++ search-service/src/main/java/com/cestc/vspace/service/impl/SearchServiceImp.java
@@ -2,7 +2,7 @@ package com.cestc.vspace.service.impl;
 
 import com.cestc.vspace.dto.Condition;
 import com.cestc.vspace.dto.PageResult;
-//import com.cestc.vspace.pojo.TbGoodsWithBLOBs;
+import com.cestc.vspace.pojo.ClothesWithBLOBS;
 import com.cestc.vspace.service.SearchService;
 import org.apache.dubbo.config.annotation.Service;
 import org.apache.solr.client.solrj.SolrClient;
@@ -47,21 +47,21 @@ public class SearchServiceImp implements SearchService {
         SolrQuery query = new SolrQuery();
         //通过查询对象设置查询条件
         //备注: 此处getSeachString()获取到的搜索字符串会被ik分词器拆分之后再进行搜索
-        query.set("q","goods_keywords:" + condition.getSearchString() + "");
+        query.set("q","cloth_keywords:" + condition.getSearchString() + "");
         //设置fq多条件过滤查询: 多条件之间使用and | or 关键字连接
-        if (condition.getCategoryIdList() != null) {
-            String categoryIdCondition = "(";
-            for (Integer integer : condition.getCategoryIdList()) {
-                categoryIdCondition += integer + " ";
-            }
-            categoryIdCondition = categoryIdCondition.substring(0,categoryIdCondition.length() - 1);
-            categoryIdCondition += ")";
-            //设置根据类型查询的查询条件
-            query.setFilterQueries("goods_category_id:" + categoryIdCondition);
-        }
+//        if (condition.getCategoryIdList() != null) {
+//            String categoryIdCondition = "(";
+//            for (Integer integer : condition.getCategoryIdList()) {
+//                categoryIdCondition += integer + " ";
+//            }
+//            categoryIdCondition = categoryIdCondition.substring(0,categoryIdCondition.length() - 1);
+//            categoryIdCondition += ")";
+//            //设置根据类型查询的查询条件
+//            query.setFilterQueries("goods_category_id:" + categoryIdCondition);
+//        }
 
         //添加商品价格过滤
-        query.addFilterQuery("goods_price:[" + condition.getMinPrice() + " TO " + condition.getMaxPrice() + "]");
+        query.addFilterQuery("cloth_price:[" + condition.getMinPrice() + " TO " + condition.getMaxPrice() + "]");
 
         //设置分页相关参数
         //①: 设置偏移量
@@ -70,21 +70,17 @@ public class SearchServiceImp implements SearchService {
         query.setRows(condition.getPageSize());
 
         //设置回显的字段(也就是查询哪些字段进行前端展示)
-        query.setFields("goods_id","goods_title","goods_price","goods_discount",
-                "goods_specification","goods_description",
-                "goods_category_id","goods_ammount",
-                "goods_image_url");
+        query.setFields("cloth_id","cloth_name","cloth_price");
 
         //排序设置: 本案例设置按照价格升序排列,再按照编号升序(相同价格的情况)
-        query.addSort("goods_price", SolrQuery.ORDER.asc);
-        query.addSort("goods_id", ORDER.asc);
+        query.addSort("cloth_price", SolrQuery.ORDER.asc);
+        query.addSort("cloth_id", ORDER.asc);
 
         //设置查询高亮结果显示
         //①: 打开高亮
         query.setHighlight(true);
         //②: 设置高亮显示的字段
-        query.addHighlightField("goods_title");
-        query.addHighlightField("goods_description");
+        query.addHighlightField("cloth_name");
         //③: 设置高亮前缀
         query.setHighlightSimplePre("<font color='red'>");
         //④: 设置高亮后缀
@@ -95,13 +91,13 @@ public class SearchServiceImp implements SearchService {
         System.out.println("solr过滤查询语句是: " + Arrays.toString(query.getFilterQueries()));
 
         //执行查询命令得到查询结果
-        List<TbGoodsWithBLOBs> goodsList = null;
+        List<ClothesWithBLOBS> goodsList = null;
         long totalItems = 0;
         int pages = 0;
         try {
             QueryResponse queryResponse = solrClient.query(query);
             //将查询结果映射成实体集合(注意该实体属性需要通过solr提供的@Field进行注解)
-            goodsList = queryResponse.getBeans(TbGoodsWithBLOBs.class);
+            goodsList = queryResponse.getBeans(ClothesWithBLOBS.class);
             //获取数据总记录数
             SolrDocumentList results = queryResponse.getResults();
             totalItems = results.getNumFound();
@@ -112,16 +108,13 @@ public class SearchServiceImp implements SearchService {
                 Map<String, Map<String, List<String>>> highlighting = queryResponse.getHighlighting();
                 if (highlighting != null) {
                     //将高亮结果循环绑定给返回结果集合
-                    for (TbGoodsWithBLOBs goods : goodsList) {
+                    for (ClothesWithBLOBS goods : goodsList) {
                         //根据商品编号获取高集合(该集合的长度由指定的高亮字段的数量决定)
                         //注意key是String类型,goodsId是Long类型,通过拼接转化成String(否则获取不了数据)
-                        Map<String, List<String>> highMap = highlighting.get(goods.getGoodsId() + "");
+                        Map<String, List<String>> highMap = highlighting.get(goods.getCid() + "");
                         if (highMap != null) {
-                            if (highMap.get("goods_title") != null) {
-                                goods.setGoodsTitle(highMap.get("goods_title").get(0));
-                            }
-                            if (highMap.get("goods_description") != null) {
-                                goods.setDescription(highMap.get("goods_description").get(0));
+                            if (highMap.get("cloth_name") != null) {
+                                goods.setCname(highMap.get("cloth_name").get(0));
                             }
                         }
                     }
---
+++ vspace-web/src/main/java/com/cestc/vspace/controller/ClothesController.java
@@ -0,0 +1,44 @@
+package com.cestc.vspace.controller;
+
+import com.cestc.vspace.dto.Condition;
+import com.cestc.vspace.dto.PageResult;
+import com.cestc.vspace.service.clothesService;
+import com.cestc.vspace.service.SearchService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zhong
+ * @Edition: V1.0
+ * CreateDate: 2021/7/15 11:42
+ * Description:
+ */
+
+@RestController
+@RequestMapping("/clothes")
+
+public class ClothesController {
+    //远程调度商品服务
+    @Reference
+    private clothesService clothesService;
+    //远程调度搜索服务
+    @Reference
+    private SearchService searchService;
+
+//    //多条件分页查询
+//    @RequestMapping("/findPageByCondition")
+//    public PageResult findPageByCondition(@RequestBody Condition condition){
+//        //根据一级级类型编号,查询出所有的二级级类型编号
+//        if (condition.getCategoryId() != 0) {
+//            condition.setCategoryIdList(categoryService.findSecondCategoryIdList(condition.getCategoryId()));
+//        }
+//        //分页查询: 此处是直接从数据库查询的数据进行分页
+////        PageResult pageResult = goodsService.findPageByCondition(condition);
+//        System.out.println("~~~~~~~~~~~~搜索关键字是: " + condition.getSearchString() + "~~~~~~~~~~~~~");
+//        //分页查询: 从solr索引库中查询
+//        PageResult pageResult = searchService.searchPage(condition);
+//        return pageResult;
+//    }
+}
