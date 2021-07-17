b949486076a55a2b1cd98a23c75b842b5aa87405
zzk <591858133@qq.com>
2021-07-17 12:14:32 +0800

    登录注册相关服务和控制器

--- userlist-api/src/main/java/com/cestc/vspace/service/UserlistService.java
+++ userlist-api/src/main/java/com/cestc/vspace/service/UserlistService.java
@@ -4,7 +4,12 @@ import com.cestc.vspace.pojo.UserList;
 
 public interface UserlistService {
 	
+	
 	UserList findById(int id);
 	
 	boolean register(UserList user);
+	
+	UserList Login(String phone,String password);
+	
+	UserList AutoLogin(int uid);
 }
--- userlist-service/src/main/java/com/cestc/vspace/service/impl/UselistServiceImpl.java
+++ userlist-service/src/main/java/com/cestc/vspace/service/impl/UselistServiceImpl.java
@@ -1,10 +1,14 @@
 package com.cestc.vspace.service.impl;
 
+import java.util.List;
+
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cestc.vspace.mapper.UserListMapper;
 import com.cestc.vspace.pojo.UserList;
+import com.cestc.vspace.pojo.UserListExample;
+import com.cestc.vspace.pojo.UserListExample.Criteria;
 import com.cestc.vspace.service.UserlistService;
 
 
@@ -13,10 +17,6 @@ public class UselistServiceImpl implements UserlistService {
 	
 	@Autowired
 	private UserListMapper usermapper;
-	
-	public UselistServiceImpl() {
-		// TODO Auto-generated constructor stub
-	}
 
 	@Override
 	public UserList findById(int id) {
@@ -29,6 +29,32 @@ public class UselistServiceImpl implements UserlistService {
 		// TODO Auto-generated method stub
 		usermapper.insert(user);
 		return true;
+	}
+
+	@Override
+	public UserList Login(String phone, String password) {
+		UserListExample example = new UserListExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andPhoneEqualTo(phone);
+		criteria.andPasswordEqualTo(password);
+		System.out.println("select phone num:");
+		System.out.println(phone);
+		System.out.println("select pwd:");
+		System.out.println(password);
+		List<UserList> list = usermapper.selectByExample(example);
+		//相当于：select * from user where username = 'wyw' and  username is null order by username asc,email desc
+		// TODO Auto-generated method stub
+		if(list.size()==0)
+			return null;
+		else
+			return list.get(0);
+	}
+
+	@Override
+	public UserList AutoLogin(int uid) {
+		UserList usr = usermapper.selectByPrimaryKey(uid);
+		// TODO Auto-generated method stub
+		return usr;
 	}
 
 }
