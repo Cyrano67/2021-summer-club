7f72547921b8c2939c0a92b5d03651fd0ac24c7c
zzk <591858133@qq.com>
2021-07-20 11:52:05 +0800

    payment相关服务类及基本配置（退款暂时没加）

---
+++ payment-service/src/main/java/com/cestc/vspace/service/conf/PaymentConfig.java
@@ -0,0 +1,113 @@
+package com.cestc.vspace.service.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application-alipay.yml")
+@ConfigurationProperties(prefix = "alipay")
+public class PaymentConfig {
+	//应用唯一表示
+	@Value("appId")
+	private String appId;
+	//应用私钥
+	@Value("privateKey")
+	private String privateKey;
+	//支付宝公钥
+	@Value("alipayPublicKey")
+	private String alipayPublicKey;
+	//异步通知跳转url
+	@Value("notifyUrl")
+	private String notifyUrl;
+	//同步通知跳转url
+	@Value("redirectUrl")
+	private String redirectUrl;
+	//数字签名类型
+	@Value("signType")
+	private String signType;
+	//编码方式
+	@Value("charset")
+	private String charset;
+	//支付宝网关
+	@Value("alipayGateway")
+	private String alipayGateway;
+	//日志存储url
+	@Value("logPath")
+	private String logPath;
+	
+	@Override
+	public String toString() {
+	return "PaymentConfig{" +
+	"appId='" + appId + '\'' +
+	", privateKey='" + privateKey + '\'' +
+	", alipayPublicKey='" + alipayPublicKey +
+	'\'' +
+	", notifyUrl='" + notifyUrl + '\'' +
+	", redirectUrl='" + redirectUrl + '\'' +
+	", signType='" + signType + '\'' +
+	", charset='" + charset + '\'' +
+	", alipayGateway='" + alipayGateway + '\''
+	+
+	", logPath='" + logPath + '\'' +
+	'}';
+	}
+
+	public String getAlipayGateway() {
+		return alipayGateway;
+		}
+	public void setAlipayGateway(String alipayGateway) {
+		this.alipayGateway = alipayGateway;
+		}
+	public String getAppId() {
+		return appId;
+		}
+	public void setAppId(String appId) {
+		this.appId = appId;
+		}
+	public String getPrivateKey() {
+		return privateKey;
+		}
+	public void setPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
+		}
+	public String getAlipayPublicKey() {
+		return alipayPublicKey;
+		}
+	public void setAlipayPublicKey(String alipayPublicKey)
+		{
+		this.alipayPublicKey = alipayPublicKey;
+		}
+	public String getNotifyUrl() {
+		return notifyUrl;
+	}
+	public void setNotifyUrl(String notifyUrl) {
+		this.notifyUrl = notifyUrl;
+	}
+	public String getRedirectUrl() {
+		return redirectUrl;
+	}
+	public void setRedirectUrl(String redirectUrl) {
+		this.redirectUrl = redirectUrl;
+	}
+	public String getSignType() {
+		return signType;
+	}
+	public void setSignType(String signType) {
+		this.signType = signType;
+	}
+	public String getCharset() {
+		return charset;
+	}
+	public void setCharset(String charset) {
+		this.charset = charset;
+	}
+	public String getLogPath() {
+		return logPath;
+	}
+	public void setLogPath(String logPath) {
+		this.logPath = logPath;
+	}
+
+}
---
+++ payment-service/src/main/java/com/cestc/vspace/service/impl/PaymentServiceImp.java
@@ -0,0 +1,75 @@
+package com.cestc.vspace.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.cestc.vspace.pojo.OrdersInfo;
+import com.cestc.vspace.service.OrdersinfoService;
+import com.cestc.vspace.service.PaymentService;
+import com.cestc.vspace.service.conf.PaymentConfig;
+
+
+@Service
+public class PaymentServiceImp implements PaymentService {
+	//自动装配一个支付配置对象
+	@Autowired
+	private PaymentConfig paymentConfig;
+	//远程调度订单服务
+	@Reference
+	private OrdersinfoService orderinfoService;
+	
+	@Override
+	public synchronized String pay(OrdersInfo trade) {
+		// TODO Auto-generated method stub
+		//创建一个支付客户端对象
+		AlipayClient alipayClient = new DefaultAlipayClient(
+			paymentConfig.getAlipayGateway(),
+			paymentConfig.getAppId(),
+			paymentConfig.getPrivateKey(),
+			"json",
+			paymentConfig.getCharset(),
+			paymentConfig.getAlipayPublicKey(),
+			paymentConfig.getSignType());
+		//创建一个支付请求对象
+		AlipayTradePagePayRequest alipayTradePagePayRequest = new AlipayTradePagePayRequest();
+		//设置请求参数
+		alipayTradePagePayRequest.setNotifyUrl(paymentConfig.getNotifyUrl());
+		alipayTradePagePayRequest.setReturnUrl(paymentConfig.getRedirectUrl());
+		//创建一个Map集合对象用于存放订单交易业务数据
+		Map<String,Object> bizParams = new HashMap<>();
+		//封装必须的业务参数即可
+		bizParams.put("out_trade_no",trade.getOrderNo()); // 商户订单号
+		bizParams.put("total_amount",String.format("%.2f",trade.getPayment()));// 交易金额,支付宝支持小数点后两位小数
+		bizParams.put("subject", "Vspace Order"); //订单标题
+		bizParams.put("body","Vspace goods list");// 对交易或商品的描述
+		bizParams.put("product_code","FAST_INSTANT_TRADE_PAY");//销售产品码
+		bizParams.put("timeout_express","30m"); // 设置支付有效时间
+		//设置支付请求需要的业务内容参数alipayTradePagePayRequest.setBizContent(JSON.toJSONString(bizParams));
+		//请求支付并获取支付结果
+		alipayTradePagePayRequest.setBizContent(JSON.toJSONString(bizParams));
+
+		String result = null;
+		try {
+			result = alipayClient.pageExecute(alipayTradePagePayRequest).getBody();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public String refund(OrdersInfo trade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
