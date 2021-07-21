5acba5dd80210080ec2d1716ce188a0d9166dfde
nfy-dot <56218632+nfy-dot@users.noreply.github.com>
2021-07-21 10:44:32 +0800

    商品数量

--- vspace-web/src/main/resources/static/js/showDetail.js
+++ vspace-web/src/main/resources/static/js/showDetail.js
@@ -3,13 +3,18 @@
 app.controller("richman-details-show-ctrl",function($scope,$controller,$http){
     //继承base_controller
     $controller("base_controller",{$scope:$scope});
-    var host = location.host;
 
     // for test,自己设置sessionStorage
     // sessionStorage.setItem("product_id",1);
 
     // 从session中得到需要打开的商品ID
     $scope.cid=sessionStorage.getItem("product_id");
+    $scope.product_num = 1;
+
+    $scope.changeNum=function(){
+        $scope.product_num = document.getElementById('product_num').value;
+        console.log($scope.product_num);
+    }
 
     $scope.initDetails = function (){
         $scope.init();
